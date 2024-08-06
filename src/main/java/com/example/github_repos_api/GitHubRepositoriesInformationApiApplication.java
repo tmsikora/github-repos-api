660fbe9c05008aecffa497624cbc57945196acbe
tmsikora <tmsikora99@gmail.com>
2024-08-06 16:44:13 +0200

    Initial commit

---
+++ src/main/java/com/example/github_repos_api/GitHubRepositoriesInformationApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.github_repos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRepositoriesInformationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubRepositoriesInformationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/github_repos_api/GitHubRepositoriesInformationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.github_repos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepositoriesInformationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
